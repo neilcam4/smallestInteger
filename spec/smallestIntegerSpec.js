16a0f0586d0a3d20e3a34cf34b12d6fbffe0eea5
Neil Campbell <neildcampbell@hotmail.com>
2019-12-07 07:14:24 +0000

    added test for negative number

--- spec/smallestIntegerSpec.js
+++ spec/smallestIntegerSpec.js
@@ -10,13 +10,23 @@ describe("Smallest Integer",function(){
         expect(result).toEqual(2)
     })
     it("should return difference of sorted array length 5", function(){
-        let A =  [3, 2, 1, 4, 5]
+        let A =  [4, 2, 4, 4, 5]
         let result = solution(A)
-        expect(result).toEqual(6)
+        expect(result).toEqual(3)
     })
     it("should return difference of sorted array length 5", function(){
         let A =  [3, 12, 1, 4, 5]
         let result = solution(A)
         expect(result).toEqual(2)
+    })
+    it("should return difference of sorted array length 5", function(){
+        let A =  [3, 12, 1, 4, 5]
+        let result = solution(A)
+        expect(result).toEqual(2)
+    })
+    it("should return 1 if negative number in array", function(){
+        let A =  [-1, -3]
+        let result = solution(A)
+        expect(result).toEqual(1)
     })
 })
--- src/smallestInteger.js
+++ src/smallestInteger.js
@@ -1,12 +1,14 @@
 function solution(A) {    
    sorted = A.sort()
-   if(sorted.length === 1){
+   if(sorted.length === 1 && sorted[0] >= 0){
        return sorted[0] + 1
    } else if (sorted.length > 1){
     let finalAnswer;
         for (var i = 0; i <= sorted.length;i++){
-            if(sorted[i+1] - sorted[i] !== 1){
-                return finalAnswer = sorted[i] + 1
+             if (sorted[i] > 0 && (sorted[i+1] - sorted[i] !== 1) ) {
+                 return finalAnswer = sorted[i] + 1
+            } else {
+                return finalAnswer= 1
             }
         }
         console.log("final answr = " + finalAnswer)
