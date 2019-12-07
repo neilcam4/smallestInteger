df8546863d525da9cbc4e2705cb57aa47ec272ef
Neil Campbell <neildcampbell@hotmail.com>
2019-12-07 06:49:45 +0000

    added tests for length 5

--- spec/smallestIntegerSpec.js
+++ spec/smallestIntegerSpec.js
@@ -1,17 +1,22 @@
 describe("Smallest Integer",function(){
-    // it("should sort numbers in order", function(){
-    //     let A = [1]
-    //     let result = solution(A)
-    //     expect(result).toEqual([1])
-    // })
-    it("should return first number of sorted array", function(){
+    it("should return next integer of one length array", function(){
+        let A = [1]
+        let result = solution(A)
+        expect(result).toEqual(2)
+    })
+    it("should return difference of sorted array length 2", function(){
         let A =  [3, 1]
         let result = solution(A)
         expect(result).toEqual(2)
     })
-    it("should return first number of sorted array", function(){
-        let A =  [2, 1, 4, 5]
+    it("should return difference of sorted array length 5", function(){
+        let A =  [3, 2, 1, 4, 5]
         let result = solution(A)
-        expect(result).toEqual(3)
+        expect(result).toEqual(6)
+    })
+    it("should return difference of sorted array length 5", function(){
+        let A =  [3, 12, 1, 4, 5]
+        let result = solution(A)
+        expect(result).toEqual(2)
     })
 })
--- src/smallestInteger.js
+++ src/smallestInteger.js
@@ -1,12 +1,15 @@
-function solution(A) {
-    sorted = A.sort()
-   
-   let finalAnswer;
-    for (var i = 0;i <= sorted.length;i++){
-        if(sorted[i+1] - sorted[i] !== 1){
-            return finalAnswer = sorted[i] + 1
+function solution(A) {    
+   sorted = A.sort()
+   if(sorted.length === 1){
+       return sorted[0] + 1
+   } else if (sorted.length > 1){
+    let finalAnswer;
+        for (var i = 0; i <= sorted.length;i++){
+            if(sorted[i+1] - sorted[i] !== 1){
+                return finalAnswer = sorted[i] + 1
+            }
         }
-    }
-    console.log("final answr = " + finalAnswer)
-    return finalAnswer
+        console.log("final answr = " + finalAnswer)
+        return finalAnswer
+   }
 }
